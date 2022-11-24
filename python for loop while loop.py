088bd4590f21196681e86d76f69984ac90b9c611
venkat1817 <dvenkataramr@gmail.com>
2022-11-24 14:36:38 +0530

    my first commit

---
+++ python for loop while loop.py
@@ -0,0 +1,85 @@
+#for loop using example program.
+#write a program to display numbers from 6 to 9.
+start_num=6
+end_num=10
+step_size=1
+for i in range(6,10,1):
+    print(i)
+# while loop using example program.
+    i=6
+    while i<10:
+        print(i)
+        i=i+1
+# 1.for loop 
+lst=[10,20,30,40,50]
+for i in lst:
+    print(i)
+#output
+10
+20
+30
+40
+50
+
+# 2.for loop
+st='python'
+for i in st:
+    print(i,end=" ")    
+#output
+    p y t h o n
+# 3.for loop
+st='python'
+for i in st:
+    print(i, end=",")    
+#output
+  p,y,t,h,o,n,
+  
+  
+# 4.for loop
+st='sai'
+for i in st:
+    print(i,end='hcl ')
+#output
+shcl ahcl ihcl
+
+
+# 5.for loop
+lst=[100,20,30,40,50]
+for i in lst:
+     print('hi')
+     print(i)
+     print('NTH')
+#output      
+hi
+10
+NTH
+
+#range topic
+1.range(1,10,2)--->output-->[2,4,6,8]
+2.range(3,12,4)--->output-->[3,7,11]
+ex: for i in range (10):
+print(i)
+out put:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+#1. wap to display all even numer up 50.
+for i in range (1,51)
+if i%2==0:
+print(i)
+
+output:
+2
+4
+8
+10
+to
+50
+
+    
